978825640b8b44e5b140c8c81dad97f7dce0de68
bakert <bakert@gmail.com>
2025-03-18 11:49:48 +0000

    Deploying to gh-pages from  @ 531a0ecea00a696423175ef46c319b29408dc3e2 🚀

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.6.8.dev9+g531a0ec',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
